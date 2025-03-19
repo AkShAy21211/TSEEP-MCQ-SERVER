97cfdef0adb6050e9f0f565c7444dae04d3464e6
Akshay P <achu21539@gmail.com>
2025-03-19 15:49:14 +0530

    feat/ added mongodb connection

---
+++ configs/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// Connect to MongoDB
+
+async function connectToDb() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/TSEEP", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+}
+
+export default connectToDb;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
-
+import connectToDb from "./configs/database";
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect to the database
+connectToDb();
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
 
 app.listen(prompt, () => {
   console.log(`Server is running on port ${PORT}`);
