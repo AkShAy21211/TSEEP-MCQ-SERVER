a991795716bd0a691d3cd6717686bba597ed81df
Akshay P <achu21539@gmail.com>
2025-03-19 20:11:33 +0530

    feat/ added cors module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv/config";
+import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import connectToDb from "./configs/database.js";
 
@@ -13,6 +14,9 @@ connectToDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Enable CORS
+app.use(cors());
 
 // Routes
 app.use("/api/auth",authRoute);
